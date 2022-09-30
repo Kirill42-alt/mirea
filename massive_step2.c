3d57e180ce058d09203aa4bcb82f55b0ce9ae8d1
Kirill42-alt <58174139+Kirill42-alt@users.noreply.github.com>
2022-10-01 01:40:31 +0300

    Add files via upload

---
+++ massive_step2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+  int a[7] = { 20, 4, -31, 3, -6, 7 };
+
+  for (int i=0; i[a]; i++) {
+      int result = ("%d, ", i[a]);
+      if (result % 2 == 0) {
+          printf("%d ", result);
+      }
+  }
+  
+}
