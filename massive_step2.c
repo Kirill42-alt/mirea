e72a173cfe47b7adb82a42bdbc179f38faf02ac5
Kirill42-alt <58174139+Kirill42-alt@users.noreply.github.com>
2022-10-10 13:13:56 +0300

    Update massive_step2.c

--- massive_step2.c
+++ massive_step2.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+
 int main(void){
+    
+  printf("%s", "Программа выводит четные числа из набора данных в массиве. \n");
+    
   int a[7] = { 20, 4, -31, 3, -6, 7 };
 
   for (int i=0; i[a]; i++) {
@@ -8,5 +12,4 @@ int main(void){
           printf("%d ", result);
       }
   }
-  
 }
