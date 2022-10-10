d1307821a5578403cb3e5387fc6b0dcf11bfa4db
Kirill42-alt <58174139+Kirill42-alt@users.noreply.github.com>
2022-10-10 13:43:20 +0300

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+#include <locale.h>
 
 int main(void) {
+    
+    setlocale(LC_ALL, "Russian");
     
     printf("%s", "Программа ищет высоту треугольника по заданным параметрам.\n");
     
     double x, y, z, p, result;
 
-    printf("Введите основание a "); scanf("%lf",&x);
-    printf("Введите сторону b "); scanf("%lf",&y);
-    printf("Введите сторону c "); scanf("%lf",&z);
+    printf("Введите основание a "); scanf_s("%lf",&x);
+    printf("Введите сторону b "); scanf_s("%lf",&y);
+    printf("Введите сторону c "); scanf_s("%lf",&z);
 
     if ((x + y >= z) && (y + z >= x) && (x + z >= y)) {
         p = (("%lf", x) + ("%lf", y) + ("%lf", z)) / 2;
